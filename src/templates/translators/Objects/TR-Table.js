5705ee694dcf4dd5ed656e66504928052a34f82f
David Maser <davidemaser@gmail.com>
2017-05-31 17:01:23 -0400

    Add some useless stuff

---
+++ src/templates/translators/Objects/TR-Table.js
@@ -0,0 +1,7 @@
+/**
+ * Created by David Maser on 31/05/2017.
+ */
+import Translate from '../Translate';
+export default class Table extends Translate{
+
+}
---
+++ src/templates/translators/Translate.js
@@ -0,0 +1,12 @@
+/**
+ * Created by David Maser on 31/05/2017.
+ */
+export default class Translate{
+  constructor(model,args){
+    this.model = model;
+    this.args = args;
+  }
+  getStructure(){
+
+  }
+}
